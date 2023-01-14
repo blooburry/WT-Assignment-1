5998a1399af882f73fbc6d8a9bd00ae1ad607c4f
blooburry <altaccstotzka031@gmail.com>
2023-01-14 17:23:45 +0100

    signin/signup form finished

---
+++ index-script.js
@@ -0,0 +1,10 @@
+function showForm(){
+    document.getElementById("#form").style.display = "flex";
+}
+function expandForm(){
+    document.getElementsByClassName("registration")[0].style.display = "block";
+}
+function closeForm(){
+    document.getElementById("#form").style.display = "none";
+    document.getElementsByClassName("registration")[0].style.display = "none";
+}
