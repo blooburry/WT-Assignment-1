4a2d7a2febda012cdc494c6de948ee7aa6af2f61
ibrahimomar9316 <120799879+ibrahimomar9316@users.noreply.github.com>
2023-01-25 22:55:52 +0100

    Update index-script.js

--- index-script.js
+++ index-script.js
@@ -1,62 +1,52 @@
-const userIDReg = /^[A-Z][a-zA-Z0-9]{4,11}[0-9|!@#\$%\^&\*]$/;
-const passReg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{12,})[a-zA-Z0-9!@#\$%\^&\*]*$/;
-const nameReg = /^[a-zA-Z]+$/;
-const zipcodeReg = /^[0-9]{4}[A-Za-z]{2}$/;
-const emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-
 function validateForm() {
-  let userID = document.getElementById("userID").value;
-  let password = document.getElementById("password").value;
-  let name = document.getElementById("name").value;
-  let zipcode = document.getElementById("zipcode").value;
-  let email = document.getElementById("email").value;
+  const userID = document.getElementById("userID").value;
+  const password = document.getElementById("password").value;
+  const name = document.getElementById("name").value;
+  const zipcode = document.getElementById("zipcode").value;
+  const email = document.getElementById("email").value;
   
-  if (!userIDReg.test(userID)) {
+  if (!/^[A-Z]/.test(userID) || !/[0-9|!@#\$%\^&\*]$/.test(userID) || userID.length < 5 || userID.length > 12) {
     alert("Invalid User ID. Must start with a capital letter, be between 5 and 12 characters long, and end with a number or special character.");
     return false;
-    }
-    else 
-    
-    if (!passReg.test(password)) {
+  }
+  
+  if (!/[a-z]/.test(password) || !/[A-Z]/.test(password) || !/[0-9]/.test(password) || !/[!@#$%^&*]/.test(password) || password.length < 12) {
     alert("Invalid password. Must be at least 12 characters long, contain a combination of uppercase letters, lowercase letters, numbers, and symbols, and cannot be a word that can be the name of a person.");
     return false;
     }
-    
-    if (!nameReg.test(name)) {
-    alert("Invalid name. Must contain the alphabet only.");
-    return false;
+    if (!/^[a-zA-Z]+$/.test(name)) {
+        alert("Invalid name.It must contain the alphabet only.");
+        return false;
+        }
+        
+        if (!/^[0-9]{4}[A-Za-z]{2}$/.test(zipcode)) {
+        alert("Invalid ZIP code. it must be a valid Netherlands postal code, consisting of four numbers and two letters.");
+        return false;
+        }
+        
+        if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/.test(email)) {
+        alert("Invalid email ID.");
+        return false;
+        }
+        
+        if (document.getElementById("sex").value == "") {
+        alert("Sex is required.");
+        return false;
+        }
+        
+        if (document.getElementById("language").value == "") {
+        alert("Language is required.");
+        return false;
+        }
+        
+        if (document.getElementById("country").value == "") {
+        alert("Country is required.");
+        return false;
+        }
+        
+        return true;
+        
     }
-    
-    if (!zipcodeReg.test(zipcode)) {
-    alert("Invalid ZIP code. Must be a valid Netherlands postal code, consisting of four numbers and two letters.");
-    return false;
-    }
-    
-    if (!emailReg.test(email)) {
-    alert("Invalid email ID.");
-    return false;
-    }
-    
-    if (document.getElementById("sex").value == "") {
-    alert("Sex field is required.");
-    return false;
-    }
-    
-    if (document.getElementById("language").value == "") {
-    alert("Language field is required.");
-    return false;
-    }
-    
-    if (document.getElementById("country").value == "") {
-    alert("Country field is required.");
-    return false;
-    }
-    
-    return true;
-    }
-    
-    
-
 
 function dynamicallyCreatedHTML(){
     generateTable();
