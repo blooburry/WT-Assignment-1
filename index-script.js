7fdd32c3e4dd144f52e5767d79c8942347cfef64
blooburry <altaccstotzka031@gmail.com>
2023-01-25 23:05:07 +0100

    just some debugging

--- index-script.js
+++ index-script.js
@@ -1,60 +1,3 @@
-const userIDRegex = /^[A-Z][a-zA-Z0-9]{4,11}[0-9|!@#\$%\^&\*]$/;
-const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=^.{12,}$).*$/;
-const nameRegex = /^[a-zA-Z]+$/;
-const zipcodeRegex = /^[0-9]{4}[A-Za-z]{2}$/;
-const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/;
-
-function validateForm() {
-let userID = document.getElementById("userID").value;
-let password = document.getElementById("password").value;
-let name = document.getElementById("name").value;
-let zipcode = document.getElementById("zipcode").value;
-let email = document.getElementById("email").value;
-
-if (!userIDRegex.test(userID)) {
-alert("Invalid User ID. Must start with a capital letter, be between 5 and 12 characters long, and end with a number or special character.");
-return false;
-}
-
-if (!passwordRegex.test(password)) {
-alert("Invalid password. Must be at least 12 characters long, contain a combination of uppercase letters, lowercase letters, numbers, and symbols, and cannot be a word that can be the name of a person.");
-return false;
-}
-
-if (!nameRegex.test(name)) {
-alert("Invalid name. Must contain the alphabet only.");
-return false;
-}
-
-if (!zipcodeRegex.test(zipcode)) {
-alert("Invalid ZIP code. Must be a valid Netherlands postal code, consisting of four numbers and two letters.");
-return false;
-}
-
-if (!emailRegex.test(email)) {
-alert("Invalid email ID.");
-return false;
-}
-
-if (document.getElementById("sex").value == "") {
-alert("Sex field is required.");
-return false;
-}
-
-if (document.getElementById("language").value == "") {
-alert("Language field is required.");
-return false;
-}
-
-if (document.getElementById("country").value == "") {
-alert("Country field is required.");
-return false;
-}
-
-return true;
-}
-
-
 function dynamicallyCreatedHTML(){
     generateTable();
 }
@@ -62,10 +5,29 @@ function dynamicallyCreatedHTML(){
 function showForm(){
     document.getElementById("login-form").style.display = "flex";
 
+    
+    document.getElementById("userID").disabled = true;
+    document.getElementById("password").disabled = true;
+    document.getElementById("name").disabled = true;
+    document.getElementById("address").disabled = true;
+    document.getElementById("zipcode").disabled = true;
+    document.getElementById("email-field").disabled = true;
+    document.getElementById("country-field").disabled = true;
+    document.getElementById("sex").disabled = true;
+    document.getElementById("language").disabled = true;
     startTimer();
 }
 function expandForm(){
     document.getElementsByClassName("registration")[0].style.display = "block";
+    document.getElementById("userID").disabled = false;
+    document.getElementById("password").disabled = false;
+    document.getElementById("name").disabled = false;
+    document.getElementById("address").disabled = false;
+    document.getElementById("zipcode").disabled = false;
+    document.getElementById("email-field").disabled = false;
+    document.getElementById("country-field").disabled = false;
+    document.getElementById("sex").disabled = false;
+    document.getElementById("language").disabled = false;
 
 }
 function closeForm(ignoreValidation){
